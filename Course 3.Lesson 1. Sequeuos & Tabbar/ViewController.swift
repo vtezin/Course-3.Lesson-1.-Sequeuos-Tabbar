0cf4d74f74f263df228769f411efebb5b662643c
Tezin Vladimir <vtezin@gmail.com>
2019-04-23 23:47:24 +0300

    creating

---
+++ Course 3.Lesson 1. Sequeuos & Tabbar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Course 3.Lesson 1. Sequeuos & Tabbar
+//
+//  Created by Admin on 23/04/2019.
+//  Copyright © 2019 Vladimir Tezin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
