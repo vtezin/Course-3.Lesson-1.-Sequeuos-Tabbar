d804ff4617b1ae0318225cacfb6abf32ad854697
Tezin Vladimir <vtezin@gmail.com>
2019-04-24 07:40:54 +0300

    added second vc

---
+++ Course 3.Lesson 1. Sequeuos & Tabbar/CheckAccessVC.swift
@@ -0,0 +1,30 @@
+//
+//  CheckAccessVC.swift
+//  Course 3.Lesson 1. Sequeuos & Tabbar
+//
+//  Created by Admin on 24/04/2019.
+//  Copyright © 2019 Vladimir Tezin. All rights reserved.
+//
+
+import UIKit
+
+class CheckAccessVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
